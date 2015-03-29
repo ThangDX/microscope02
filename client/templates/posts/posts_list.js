2d1e00b49aadaf07125929edfd5cdc2389df3cb6
Thang Dang <dangxuanthang@gmail.com>
2015-03-29 21:15:47 +0700

    Commit 4-3 Wired collection into 'postList' template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,22 +1,5 @@
-/**
- * Created by thangdx on 24/03/2015.
- */
-
-var postData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
-
 Template.postsList.helpers({
-    posts: postData
+    posts: function () {
+        return Posts.find();
+    }
 });
